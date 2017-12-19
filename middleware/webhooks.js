1344d820cd91602da94ee1be405a5d9414d4e104
Ivan Linos <ivan@linos.es>
2017-12-19 16:39:55 +0100

    fixed webhooks and sqlStrategy

--- middleware/webhooks.js
+++ middleware/webhooks.js
@@ -1,44 +1,46 @@
 const crypto = require('crypto');
 const getRawBody = require('raw-body');
 
-module.exports = function configureWithWebhook({ secret, shopStore }) {
-  return function createWebhookHandler(onVerified) {
-    return async function withWebhook(request, response) {
-      const { body: data } = request;
-      const hmac = request.get('X-Shopify-Hmac-Sha256');
-      const topic = request.get('X-Shopify-Topic');
-      const shopDomain = request.get('X-Shopify-Shop-Domain');
+module.exports = function createWithWebhook({ secret, shopStore }) {
+    return function withWebhook(request, response, next) {
+        const { body: data } = request;
+        const hmac = request.get('X-Shopify-Hmac-Sha256');
+        const topic = request.get('X-Shopify-Topic');
+        const shopDomain = request.get('X-Shopify-Shop-Domain');
 
-      try {
-        const rawBody = await getRawBody(request);
-        const generated_hash = crypto
-          .createHmac('sha256', secret)
-          .update(rawBody)
-          .digest('base64');
+        try {
+            getRawBody(request)
+                .then(buf => {
+                    const generated_hash = crypto
+                        .createHmac('sha256', secret)
+                        .update(buf)
+                        .digest('base64');
 
-        if (generated_hash !== hmac) {
-          response.status(401).send();
-          onVerified(new Error("Unable to verify request HMAC"));
-          return;
+                    if (generated_hash !== hmac) {
+                        response.status(401).send();
+                        throw new Error("Unable to verify request HMAC");
+                        return;
+                    }
+
+                    shopStore.getShop({ shop: shopDomain }, (error, { accessToken }) => {
+                        if (error) {
+                            response.status(401).send();
+                            throw new Error("Couldn't fetch credentials for shop");
+                            return;
+                        }
+
+                        request.body = buf.toString('utf8');
+                        request.webhook = { topic, shopDomain, accessToken };
+
+                        //response.status(200).send();
+                        next();
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } catch(error) {
+            response.send(error);
         }
-
-        shopStore.getShop({ shop: shopDomain }, (error, { accessToken }) => {
-          if (error) {
-            response.status(401).send();
-            onVerified(new Error("Couldn't fetch credentials for shop"));
-            return;
-          }
-
-          request.body = rawBody.toString('utf8');
-          request.webhook = { topic, shopDomain, accessToken };
-
-          response.status(200).send();
-
-          onVerified(null, request);
-        });
-      } catch(error) {
-        response.send(error);
-      }
     };
-  }
 };
--- strategies/SQLStrategy.js
+++ strategies/SQLStrategy.js
@@ -1,46 +1,59 @@
 const Knex = require('knex');
 
 const defaultConfig = {
-  dialect: 'sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: './db.sqlite3',
-  },
+    dialect: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+        filename: './db.sqlite3',
+    },
 };
 
 module.exports = class SQLStrategy {
-  constructor(config = defaultConfig) {
-    this.knex = Knex(config);
-  }
+    constructor(config = defaultConfig) {
+        this.knex = Knex(config);
+    }
 
-  initialize() {
-    return this.knex.schema
-      .createTableIfNotExists('shops', table => {
-        table.increments('id');
-        table.string('shopify_domain');
-        table.string('access_token');
-        table.unique('shopify_domain');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    initialize() {
+        return this.knex.schema
+            .createTableIfNotExists('shops', table => {
+                table.increments('id');
+                table.string('shopify_domain');
+                table.string('access_token');
+                table.unique('shopify_domain');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-  storeShop({ shop, accessToken, data = {} }, done) {
-    this.knex
-      .raw(
-        `INSERT OR IGNORE INTO shops (shopify_domain, access_token) VALUES ('${shop}', '${accessToken}')`
-      )
-      .then(result => {
-        return done(null, accessToken);
-      });
-  }
+    storeShop({shop, accessToken, data = {}}, done) {
+        let sqlRaw = '';
 
-  getShop({ shop }, done) {
-    this.knex('shops')
-      .where('shopify_domain', shop)
-      .then(result => {
-        return done(null, result);
-      });
-  }
+        switch (this.knex.client.config.client) {
+            case 'mysql':
+                sqlRaw = `INSERT IGNORE INTO shops (shopify_domain, access_token) VALUES ('${shop}', '${accessToken}')`;
+                break;
+            case 'pg':
+                sqlRaw = `INSERT INTO shops (shopify_domain, access_token) VALUES ('${shop}', '${accessToken}') ON CONFLICT DO NOTHING`;
+                break;
+            case 'sqlite3':
+                sqlRaw = `INSERT OR IGNORE INTO shops (shopify_domain, access_token) VALUES ('${shop}', '${accessToken}')`;
+                break;
+            default:
+                return;
+        }
+
+        this.knex.raw(sqlRaw)
+            .then(result => {
+                return done(null, accessToken);
+            });
+    }
+
+    getShop({shop}, done) {
+        this.knex('shops')
+            .where('shopify_domain', shop)
+            .then(result => {
+                return done(null, result);
+            });
+    }
 };
